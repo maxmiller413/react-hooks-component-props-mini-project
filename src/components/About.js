93f0448079bdafe53ff926f17852679320192626
Max Miller <maxmiller413@gmail.com>
2021-04-06 17:19:28 -0500

    Done.

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About({ about, image = "https://via.placeholder.com/215" }) {
+    return (
+        <aside>
+            <img src={image} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    );
+}
+
+export default About;
+
+// import React from "react";
+
+// function About({ about, image = "https://via.placeholder.com/215" }) {
+//   return (
+//     <aside>
+//       <img src={image} alt="blog logo" />
+//       <p>{about}</p>
+//     </aside>
+//   );
+// }
+
+// export default About;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}) {
+
+    const articleArr = posts.map((post) => (
+            <Article 
+                key={post.id}
+                title={post.title}
+                date={post.date}
+                preview={post.preview}
+                minutes={post.minutes}
+            />
+        ));
+        return <main>{articleArr}</main>
+}
+
+export default ArticleList;
